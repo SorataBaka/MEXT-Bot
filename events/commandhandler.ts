2404b5102873ff7b8fa558a318db8f4978fdabd5
Christian Harjuno <harjuno.tian@gmail.com>
2022-02-17 21:35:57 +0700

    fixed bug where uppercase prefixes are not detected

--- events/commandhandler.ts
+++ events/commandhandler.ts
@@ -7,7 +7,7 @@ module.exports = {
     if(message.member?.user.bot) return
     const prefix = await client.ClientFunction.getprefix(client, message.guild?.id) || client.PREFIX
     if (message.author.bot) return;
-    if (message.content.indexOf(prefix) !== 0) return;
+    if (message.content.toLowerCase().indexOf(prefix.toLowerCase()) !== 0) return;
 
     let args:string[] | string = message.content.slice(prefix.length).trim().split(/ +/g);
     const commandName = args[0].toLowerCase();
